e56521819ec580272aaffc50aa50cea62c3ea2fd
p1xle <wizmonkey1337@gmail.com>
2025-01-18 12:47:51 +0530

    updated listmaps

--- src/data/ListMaps.ts
+++ src/data/ListMaps.ts
@@ -1,53 +1,53 @@
 export const listOptions = [
-    { value: 'backlog', label: 'Backlog' },
-    { value: 'wishlist', label: 'Wishlist' },
-    { value: 'playlist', label: 'Playing' },
-    { value: 'completed', label: 'Completed' },
-    { value: 'dropped', label: 'Dropped' },
+  {value: 'backlog', label: 'Backlog'},
+  {value: 'wishlist', label: 'Wishlist'},
+  {value: 'playlist', label: 'Playing'},
+  {value: 'completed', label: 'Completed'},
+  {value: 'dropped', label: 'Dropped'},
 ];
 
-export const listLabels: { [key: string]: string } = {
-    backlog: 'Backlog',
-    wishlist: 'Wishlist',
-    playlist: 'Playing',
-    completed: 'Completed',
-    dropped: 'Dropped',
+export const listLabels: {[key: string]: string} = {
+  backlog: 'Backlog',
+  wishlist: 'Wishlist',
+  playlist: 'Playing',
+  completed: 'Completed',
+  dropped: 'Dropped',
 };
 
-export const listIcons: { [key: string]: string } = {
-    backlog: 'progress-alert',
-    playlist: 'motion-play-outline',
-    wishlist: 'bookmark-outline',
-    completed: 'checkbox-marked-circle-outline',
-    dropped: 'minus-circle-outline',
+export const listIcons: {[key: string]: string} = {
+  backlog: 'progress-alert',
+  playlist: 'motion-play-outline',
+  wishlist: 'bookmark-outline',
+  completed: 'checkbox-marked-circle-outline',
+  dropped: 'minus-circle-outline',
 };
 
-export const listDesc: { [key: string]: string } = {
-    backlog: 'Games to Play',
-    playlist: 'Currently Playing',
-    wishlist: 'Wishlisted Games',
-    completed: 'Completed Games',
-    dropped: 'Dropped/Ignored Games',
+export const listDesc: {[key: string]: string} = {
+  backlog: 'Games to Play',
+  playlist: 'Currently Playing',
+  wishlist: 'Wishlisted Games',
+  completed: 'Completed Games',
+  dropped: 'Dropped/Ignored Games',
 };
 
-export const listColors: { [key: string]: string } = {
-    backlog: '#FA5F55',
-    playlist: '#89CFF0',
-    wishlist: '#F33A6A',
-    completed: '#0BDA51',
-    dropped: '#F4C430',
+export const listColors: {[key: string]: string} = {
+  backlog: '#FA5F55',
+  playlist: '#89CFF0',
+  wishlist: '#F33A6A',
+  completed: '#0BDA51',
+  dropped: '#F4C430',
 };
 
-export const listSort: { [key: string]: string } = {
-    name: 'Name',
-    addedDate: 'Date Added',
-    listName: 'List',
+export const listSort: {[key: string]: string} = {
+  name: 'Name',
+  addedDate: 'Date Added',
+  listName: 'List',
 };
 
-export const dashList: { [key: string]: string } = {
-    backlog: 'Games to Play Next',
-    playlist: 'Currently Playing',
-    completed: 'Recently Completed',
-    wishlist: 'Wishlisted',
-    dropped: 'Dropped/Ignored Games'
+export const dashList: {[key: string]: string} = {
+  backlog: 'Games to Play Next',
+  playlist: 'Currently Playing',
+  completed: 'Recently Completed',
+  wishlist: 'Wishlisted',
+  dropped: 'Dropped/Ignored Games',
 };
