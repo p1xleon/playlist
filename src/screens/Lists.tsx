29237736962b1b4a7fb2574afaa9b8be70b8b121
p1xle <wizmonkey1337@gmail.com>
2025-01-18 12:55:04 +0530

    optimized lists

--- src/screens/Lists.tsx
+++ src/screens/Lists.tsx
@@ -1,47 +1,60 @@
-import { Text, View, FlatList, Image, Pressable, TouchableOpacity, Alert } from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  Image,
+  Pressable,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 
 //firebase
 import auth from '@react-native-firebase/auth';
-import { deleteGameFromList, getCurrentUser, getUserLists, moveGameToList } from '../services/auth/firebase';
-import { ReGame } from '../services/auth/firebase';
+import {
+  deleteGameFromList,
+  getCurrentUser,
+  getUserLists,
+  moveGameToList,
+} from '../services/auth/firebase';
+import {ReGame} from '../services/auth/firebase';
 
 //navigation
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../routes/Navigator';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../routes/Navigator';
 
 //hooks and utils
-import { getRelativeTime } from '../utils/dateTime';
-import { listIcons, listDesc, listColors } from '../data/ListMaps';
+import {getRelativeTime} from '../utils/dateTime';
+import {listIcons, listDesc, listColors} from '../data/ListMaps';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 //components
-import { SceneMap, TabBar, TabView } from 'react-native-tab-view';
-import { Loading } from '../components/Loading';
+import {SceneMap, TabBar, TabView} from 'react-native-tab-view';
+import {Loading, OverlayLoader} from '../components/Loading';
 import SearchBar from '../components/SearchBar';
-import Sheet, { SheetHandle } from '../components/ActionSheet';
+import Sheet, {SheetHandle} from '../components/ActionSheet';
 import Snackbar from 'react-native-snackbar';
-import { Separator } from '../components/Utils';
+import {Separator} from '../components/Utils';
 import Header from '../components/Header';
 
-import { pxStyles } from '../theme/useTheme';
+import {pxStyles} from '../theme/useTheme';
 
 type ListsProps = NativeStackScreenProps<RootStackParamList, 'Lists'>;
 
+const initialLayout = {width: 500};
 
-const initialLayout = { width: 500 };
-
-const Lists = ({ navigation }: ListsProps) => {
+const Lists = ({navigation}: ListsProps) => {
   const styles = useStyles();
 
   const [lists, setLists] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [index, setIndex] = useState(0);
-  const [routes, setRoutes] = useState<{ key: string; title: string }[]>([]);
+  const [routes, setRoutes] = useState<{key: string; title: string}[]>([]);
   const [selectedGame, setSelectedGame] = useState<ReGame | null>(null);
   const [sourceList, setSourceList] = useState<string | null>(null);
+  const [isProcessing, setIsProcessing] = useState(false);
 
   const sheetRef = useRef<SheetHandle>(null);
 
@@ -52,7 +65,7 @@ const Lists = ({ navigation }: ListsProps) => {
     if (currentUser) {
       const userId = auth().currentUser?.uid;
       if (userId) {
-        const unsubscribe = getUserLists(userId, (listsData) => {
+        const unsubscribe = getUserLists(userId, listsData => {
           setLists(listsData);
 
           // Set routes based on the lists
@@ -62,7 +75,13 @@ const Lists = ({ navigation }: ListsProps) => {
           }));
 
           const orderRoutes = listRoutes.sort((a, b) => {
-            const order = ['backlog', 'playing', 'wishlist', 'completed', 'dropped'];
+            const order = [
+              'backlog',
+              'playing',
+              'wishlist',
+              'completed',
+              'dropped',
+            ];
             return order.indexOf(a.key) - order.indexOf(b.key);
           });
 
@@ -79,9 +98,10 @@ const Lists = ({ navigation }: ListsProps) => {
   //move game to between lists
   const handleMoveGame = async (targetList: string) => {
     if (selectedGame && sourceList) {
+      setIsProcessing(true);
       try {
         if (userId) {
-          const updatedGame = { ...selectedGame, addedDate: new Date() };
+          const updatedGame = {...selectedGame, addedDate: new Date()};
           await moveGameToList(userId, sourceList, targetList, updatedGame);
           sheetRef.current?.dismiss();
           Snackbar.show({
@@ -92,13 +112,20 @@ const Lists = ({ navigation }: ListsProps) => {
           setSelectedGame(null);
         }
       } catch (error) {
-        // console.error('Failed to move game', error);
+        Snackbar.show({
+          text: 'Failed to move the game',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+      } finally {
+        setIsProcessing(false);
       }
     }
   };
 
+  //delete game from collection
   const handleDeleteGame = async () => {
     if (selectedGame && sourceList) {
+      setIsProcessing(true);
       try {
         const userId = auth().currentUser?.uid;
         if (userId) {
@@ -112,70 +139,116 @@ const Lists = ({ navigation }: ListsProps) => {
           setSelectedGame(null);
         }
       } catch (error) {
-        // console.error('Failed to delete game', error);
+        Snackbar.show({
+          text: 'Failed to delete the game',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+      } finally {
+        setIsProcessing(false);
       }
     }
   };
 
-  //filter sourcelist from other lists to show in action sheet
-  const moveLists = lists.filter(list => list.id !== sourceList);
-
-  const renderScene = SceneMap(
-    routes.reduce((scenes, route) => {
-      const list = lists.find(list => list.id === route.key);
-      const colors = listColors[route.key];
-      scenes[route.key] = () => (
-        <View style={styles.container}>
-          <View style={styles.listInfo}>
-            <View style={{ flexDirection: 'row' }}>
-              <Icon name={listIcons[route.key]} size={24} color={colors} />
-              <Text style={[styles.title, { fontSize: 18, marginLeft: 7, color: colors }]}>{listDesc[route.key]}</Text>
-            </View>
-            <Text style={styles.count}>{list?.gameCount || null}</Text>
-          </View>
-          <FlatList
-            data={lists.find(list => list.id === route.key)?.games || []}
-            keyExtractor={(game) => game.id.toString()}
-            style={{ marginBottom: 60 }}
-            renderItem={({ item: game }: { item: ReGame }) => (
-              <View>
-                <TouchableOpacity
-                  style={styles.gameCard}
-                  onPress={() => {
-                    const { addedDate, ...gameDetails } = game;
-                    navigation.navigate('GameDetails', { game: gameDetails });
-                  }}
-                  onLongPress={() => {
-                    setSelectedGame(game);
-                    setSourceList(route.key);
-                    sheetRef.current?.present();
-                  }}>
-                  <Image source={{ uri: game.background_image }} style={{ width: 50, height: 50, borderRadius: 1, marginRight: 10 }} />
-                  <View style={styles.gameDetails}>
-                    <Text style={styles.title} numberOfLines={1}>{game.name}</Text>
-                    <Text style={styles.addedDate}>{`Added ${getRelativeTime(new Date(game.addedDate))}`}</Text>
-                  </View>
-                  <Pressable onPress={() => {
-                    setSelectedGame(game);
-                    setSourceList(route.key);
-                    sheetRef.current?.present();
-                  }}>
-                    <Icon name="dots-vertical" color='#fff' size={24} style={[styles.icon, { paddingBottom: 10, paddingLeft: 10, paddingRight: 1}]} />
-                  </Pressable>
-                </TouchableOpacity>
-              </View>
-            )}
+  const memoizedRenderItem = React.useCallback(
+    ({item: game}: {item: ReGame}, listKey: string) => (
+      <View>
+        <TouchableOpacity
+          style={styles.gameCard}
+          onPress={() => {
+            const {addedDate, ...gameDetails} = game;
+            navigation.navigate('GameDetails', {game: gameDetails});
+          }}>
+          <Image
+            source={{uri: game.background_image}}
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 1,
+              marginRight: 10,
+            }}
           />
-        </View>
-      );
-      return scenes;
-    }, {} as any)
+          <View style={styles.gameDetails}>
+            <Text style={styles.title} numberOfLines={1}>
+              {game.name}
+            </Text>
+            <Text style={styles.addedDate}>{`Added ${getRelativeTime(
+              new Date(game.addedDate),
+            )}`}</Text>
+          </View>
+          <Pressable
+            onPress={() => {
+              setSelectedGame(game);
+              setSourceList(listKey);
+              sheetRef.current?.present();
+            }}>
+            <Icon
+              name="dots-vertical"
+              color="#fff"
+              size={24}
+              style={[
+                styles.icon,
+                {paddingBottom: 10, paddingLeft: 10, paddingRight: 1},
+              ]}
+            />
+          </Pressable>
+        </TouchableOpacity>
+      </View>
+    ),
+    [
+      navigation,
+      styles.icon,
+      styles.gameCard,
+      styles.gameDetails,
+      styles.title,
+    ],
   );
+
+  const renderScene = ({route}: {route: {key: string; title: string}}) => {
+    const list = lists.find(list => list.id === route.key);
+    const colors = listColors[route.key];
+    const listData = lists.find(list => list.id === route.key)?.games || [];
+    const sortedGames = listData.sort((a: ReGame,b: ReGame) => {
+      return new Date(b.addedDate).getTime() - new Date(a.addedDate).getTime();
+    })
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.listInfo}>
+          <View style={{flexDirection: 'row'}}>
+            <Icon name={listIcons[route.key]} size={24} color={colors} />
+            <Text
+              style={[
+                styles.title,
+                {fontSize: 18, marginLeft: 7, color: colors},
+              ]}>
+              {listDesc[route.key]}
+            </Text>
+          </View>
+          <Text style={styles.count}>{list?.gameCount || null}</Text>
+        </View>
+        <FlatList
+          data={sortedGames}
+          keyExtractor={item => item.id.toString()}
+          style={{marginBottom: 60}}
+          renderItem={({item}) => memoizedRenderItem({item}, route.key)}
+          maintainVisibleContentPosition={{
+            minIndexForVisible: 0,
+          }}
+          getItemLayout={(data, index) => ({
+            length: 50,
+            offset: 50 * index,
+            index,
+          })}
+          initialNumToRender={9}
+        />
+      </View>
+    );
+  };
 
   const renderTabBar = (props: any) => (
     <View style={styles.container}>
-      <View style={{ paddingHorizontal: 10 }}>
-        <Header title='Your Lists' />
+      <View style={{paddingHorizontal: 10}}>
+        <Header title="Your Lists" />
         <SearchBar />
       </View>
       <TabBar
@@ -184,52 +257,55 @@ const Lists = ({ navigation }: ListsProps) => {
         tabStyle={styles.tab}
         labelStyle={styles.label}
         indicatorStyle={styles.indicator}
-        inactiveColor='#919191'
-        renderLabel={({ route, color }) => {
+        inactiveColor="#919191"
+        renderLabel={({route, color}) => {
           const list = lists.find(list => list.id === route.key);
           return (
-            <Text
-              style={[styles.label, { color }]}
-              numberOfLines={1}>
+            <Text style={[styles.label, {color}]} numberOfLines={1}>
               {route.title}
             </Text>
-          )
+          );
         }}
       />
 
       {selectedGame && (
-        <Sheet ref={sheetRef} title={selectedGame.name || ''}
-        onDismiss={() => {
-          setSelectedGame(null);
-          setSourceList(null);
-        }}>
-          <View style={{ paddingHorizontal: 10 }}>
-            <Separator style={{ backgroundColor: '#494949' }} />
+        <Sheet ref={sheetRef} title={selectedGame.name}>
+          {isProcessing && <OverlayLoader />}
+          <View style={{paddingHorizontal: 10}}>
+            <Separator style={{backgroundColor: '#494949'}} />
             <View>
-              {moveLists.map(list => (
-                <TouchableOpacity
-                  key={list.id}
-                  onPress={() => handleMoveGame(list.id)}
-                  style={styles.actionListItem}
-                >
-                  <Icon name={listIcons[list.id] || 'progress-question'} size={24} color={listColors[list.id]} />
-                  <Text style={styles.actionListText}>
-                    <Text style={styles.title}>Move to </Text>
-                    {list.id.charAt(0).toUpperCase() + list.id.slice(1)}</Text>
-                </TouchableOpacity>
-              ))}
+              {lists
+                .filter(list => list.id !== sourceList)
+                .map(list => (
+                  <TouchableOpacity
+                    key={list.id}
+                    disabled={isProcessing}
+                    onPress={() => handleMoveGame(list.id)}
+                    style={[styles.actionListItem, isProcessing && {opacity: 0.5}]} >
+                    <Icon
+                      name={listIcons[list.id] || 'progress-question'}
+                      size={24}
+                      color={listColors[list.id]}
+                    />
+                    <Text style={styles.actionListText}>
+                      <Text style={styles.title}>Move to </Text>
+                      {list.id.charAt(0).toUpperCase() + list.id.slice(1)}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
             </View>
-            <Separator style={{ backgroundColor: '#494949' }} />
-            <TouchableOpacity style={styles.actionListItem} onPress={handleDeleteGame}>
-              <Icon name='delete-outline' size={24} color='#f32929' />
-              <Text style={styles.actionListText}>Delete from Collection</Text>
+            <Separator style={{backgroundColor: '#494949'}} />
+            <TouchableOpacity
+              style={styles.actionListItem}
+              onPress={handleDeleteGame}>
+              <Icon name="delete-outline" size={24} color="#f32929" />
+              <Text style={[styles.actionListText, isProcessing && {opacity: 0.5}]}>Delete from Collection</Text>
             </TouchableOpacity>
           </View>
         </Sheet>
       )}
     </View>
   );
-
 
   if (loading) {
     return <Loading />;
@@ -241,8 +317,8 @@ const Lists = ({ navigation }: ListsProps) => {
 
   return (
     <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
+      navigationState={{index, routes}}
+      renderScene={renderScene} // Pass renderScene directly
       onIndexChange={setIndex}
       renderTabBar={renderTabBar}
       initialLayout={initialLayout}
@@ -251,7 +327,7 @@ const Lists = ({ navigation }: ListsProps) => {
   );
 };
 
-const useStyles = pxStyles((theme) => ({
+const useStyles = pxStyles(theme => ({
   container: {
     backgroundColor: theme.background,
   },
@@ -265,7 +341,7 @@ const useStyles = pxStyles((theme) => ({
   },
   gameDetails: {
     flex: 1,
-    marginTop: -5
+    marginTop: -5,
   },
   title: {
     color: theme.primary,
@@ -281,13 +357,13 @@ const useStyles = pxStyles((theme) => ({
     backgroundColor: theme.background,
   },
   tab: {
-    paddingHorizontal: -20 //fix tab bar label text truncating
+    paddingHorizontal: -20, //fix tab bar label text truncating
   },
   listInfo: {
     flexDirection: 'row',
     margin: 10,
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   label: {
     fontWeight: '900',
@@ -296,7 +372,7 @@ const useStyles = pxStyles((theme) => ({
   count: {
     color: theme.secondary,
     fontWeight: '900',
-    marginRight: 10
+    marginRight: 10,
   },
   indicator: {
     backgroundColor: 'gold',
@@ -312,11 +388,11 @@ const useStyles = pxStyles((theme) => ({
     color: theme.primary,
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 10
+    marginLeft: 10,
   },
   icon: {
-    color: theme.primary
-  }
+    color: theme.primary,
+  },
 }));
 
 export default Lists;
