2af1c1cfb5731a612233154674aed768cb59d9dd
p1xle <wizmonkey1337@gmail.com>
2025-01-18 12:46:14 +0530

    updated loaders

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -34,6 +34,15 @@ export const Loader = () => {
   )
 }
 
+export const OverlayLoader = () => {
+  const styles = useStyles();
+  return (
+    <View style={styles.ReLoader}>
+      <ActivityIndicator size={30} color={'#ffd100'} />
+    </View>
+  )
+}
+
 const useStyles = pxStyles((theme) => ({
   loader: {
     flex: 1,
@@ -51,4 +60,15 @@ const useStyles = pxStyles((theme) => ({
     alignItems: 'center',
     padding: 0
   },
+  overlayLoader: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1000,
+  }
 }));
